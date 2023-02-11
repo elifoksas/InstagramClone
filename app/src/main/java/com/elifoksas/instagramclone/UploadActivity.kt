1b5efa4c75de34af63ac90d37776487471865510
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-02-11 13:10:47 +0300

    initial commit

--- app/src/main/java/com/elifoksas/instagramclone/UploadActivity.kt
+++ app/src/main/java/com/elifoksas/instagramclone/UploadActivity.kt
@@ -3,11 +3,19 @@ package com.elifoksas.instagramclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.elifoksas.instagramclone.databinding.ActivityMainBinding
+import com.elifoksas.instagramclone.databinding.ActivityUploadBinding
 
 class UploadActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUploadBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
 
