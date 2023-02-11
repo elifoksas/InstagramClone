b5bd0f9d8a98a67e1458aed44eaa3747f2d1c8f6
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-02-11 14:02:01 +0300

    Menu Added

--- app/src/main/java/com/elifoksas/instagramclone/FeedActivity.kt
+++ app/src/main/java/com/elifoksas/instagramclone/FeedActivity.kt
@@ -1,12 +1,19 @@
 package com.elifoksas.instagramclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.elifoksas.instagramclone.databinding.ActivityFeedBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFeedBinding
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +22,33 @@ class FeedActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        auth = Firebase.auth
 
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.insta_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_post){
+            val intent = Intent(this,UploadActivity::class.java)
+            startActivity(intent)
+
+        }else if (item.itemId == R.id.sign_out){
+
+            auth.signOut()
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
