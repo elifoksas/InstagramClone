dace7f26e415d2e5dd6ef8cfd7d35a82185ebfbd
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-02-11 13:11:04 +0300

    signUp method

--- app/src/main/java/com/elifoksas/instagramclone/MainActivity.kt
+++ app/src/main/java/com/elifoksas/instagramclone/MainActivity.kt
@@ -1,22 +1,60 @@
 package com.elifoksas.instagramclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.elifoksas.instagramclone.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = Firebase.auth
 
 
     }
+
 
     fun signInClicked(view: View){
 
     }
 
     fun signUpClicked(view: View){
+
+        val email = binding.editTextTextEmailAddress.text.toString()
+        val password = binding.editTextPassword.text.toString()
+
+        if(email.equals("") || password.equals("")){
+
+            Toast.makeText(this,"Enter email and password.",Toast.LENGTH_LONG).show()
+
+
+        }else{
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener {
+                //success
+                val intent = Intent(this@MainActivity,FeedActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }.addOnFailureListener {
+                //failure
+                Toast.makeText(this@MainActivity,it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+
+        }
 
     }
 }
