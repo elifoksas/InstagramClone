590121bd131afd0e94fd406328af1de193fac136
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-02-12 16:23:30 +0300

    ordered posts by date

--- app/src/main/java/com/elifoksas/instagramclone/view/FeedActivity.kt
+++ app/src/main/java/com/elifoksas/instagramclone/view/FeedActivity.kt
@@ -14,6 +14,7 @@ import com.elifoksas.instagramclone.model.Post
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -46,7 +47,7 @@ class FeedActivity : AppCompatActivity() {
 
     private fun getData(){
 
-        db.collection("Posts").addSnapshotListener { value, error ->
+        db.collection("Posts").orderBy("date", Query.Direction.DESCENDING).addSnapshotListener { value, error ->
 
             if (error != null){
                 Toast.makeText(this,error.localizedMessage,Toast.LENGTH_LONG).show()
